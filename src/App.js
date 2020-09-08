3b707c9fc7db75f0576c842b540aa03ca57636dc
nomanme <abdullah.noman083@gmail.com>
2020-09-08 22:16:35 +0600

    err

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route path="/" component={Default}></Route>
+      </Switch>
+      {/* <ProductList></ProductList>
+      <Details></Details>
+      <Cart></Cart> */}
+      {/* <Default></Default> */}
+    </React.Fragment>
+  );
+}
+
+// router install
+// npm install --save react-router-dom
+
+export default App;
