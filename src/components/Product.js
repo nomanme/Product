cec2d273ab20d2420e9ba408d5d62b4fb967122c
nomanme <abdullah.noman083@gmail.com>
2020-09-09 03:06:01 +0600

    cart button design

--- src/components/Product.js
+++ src/components/Product.js
@@ -44,7 +44,59 @@ class Product extends Component {
     );
   }
 }
-const ProductWrapper = styled.div`
 
+const ProductWrapper = styled.div`
+.card{
+  border-color: transparent;
+  transition: all 1s linear;
+  background-color: lightgray;
+}
+.card-footer{
+  background: transparent;
+  border-top:transparent;
+  transition: all 1s linear;
+  // background-color:tomato;
+  color:crimson;
+}
+&:hover{
+  .card{
+    border: 0.04rem solid rgba(0, 0, 0, 0.2); 
+    box-shadow: 2px 2px 5px 0px (0, 0, 0, 0.2);
+  }
+  .card-footer{
+    background: rgba(247,247,247);
+  }
+}
+.img-container{
+  position:relative;
+  overflow:hidden;
+}
+.card-img-top{
+  transition: all 1s linear;
+}
+.img-container:hover .card-img-top{
+  transform: scale(1.4);
+}
+.cart-btn{
+  position:absolute;
+  bottom:0;
+  right:0;
+  margin:0;
+  padding: 0.2rem 0.4rem;
+  background:var(--lightBlue);
+  border:none;
+  color: var(--mainWhite);
+  font-size:1.4rem;
+  border-radius:0.5rem 0 0 0;
+  transform: translate(100%,100%);
+  transition: all 0.30s linear;
+}
+.img-container:hover .cart-btn{
+transform:translate(0 , 0);
+}
+.cart-btn:hover{
+  color:var(--mainBlue);
+  cursor:pointe;
+}
 `
 export default Product;
