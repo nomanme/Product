f2552ca0265333369419e5cdfd0d9e1c78ccfb83
nomanme <abdullah.noman083@gmail.com>
2020-09-09 04:19:31 +0600

    details page setting

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { ProductConsumer } from "../Context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
 
 class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>This is details</h3>
-      </div>
+      <ProductConsumer>
+        {
+          (value) => {
+            const { id, company } = value.detailProduct;
+          }
+        }
+      </ProductConsumer>
     );
   }
 }
