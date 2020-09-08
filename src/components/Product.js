ec04c3fa0601c61cd05c5532a56af0457f1faa6c
nomanme <abdullah.noman083@gmail.com>
2020-09-09 01:50:06 +0600

    product setup

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,11 +8,20 @@ class Product extends Component {
     const { id, title, img, price, inCart } = this.props.product
     return (
 
-      <div>
+      <ProductWrapper className="col-9 mx-auto col-6 mx auto col-lg-3 my-3">
         <h3> This is Product</h3>
-      </div >
+        <div className="card">
+          <div className="img-container p-5" onClick={console.log("you clicked me")}>
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+          </div>
+        </div>
+      </ProductWrapper >
     );
   }
 }
+const ProductWrapper = styled.div`
 
+`
 export default Product;
